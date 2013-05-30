2fd7e5520f273d575d354b84793faf36304eb50b
Earth <shungo_kaetsu@yahoo.co.jp>
2013-05-30 08:52:24 +0800

    modified friend code

--- accu/overload/c++_friend.cpp
+++ accu/overload/c++_friend.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+// friend class
 class Storage {
 private:
   int m_nValue;
@@ -30,11 +31,40 @@ public:
   }
 };
 
+// friend function of more than one class
+class Humidity;
+
+class Temperature {
+private:
+  int m_nTmp;
+public:
+  Temperature(int nTmp) { m_nTmp = nTmp; }
+  friend void PrintWeather(Temperature &cTemperature, Humidity &cHumidity);
+};
+
+class Humidity {
+private:
+  int m_nHumidity;
+public:
+  Humidity(int nHumidity) { m_nHumidity = nHumidity; }
+  friend void PrintWeather(Temperature &cTemperature, Humidity &cHumidity);
+};
+
+void PrintWeather(Temperature &cTemperature, Humidity &cHumidity) {
+  std::cout << "The temperature is " << cTemperature.m_nTmp <<
+    " and the humidity is " << cHumidity.m_nHumidity << std::endl;
+}
+
 int main() {
   Storage cStorage(5, 6.7);
   Display cDisplay(false);
 
   cDisplay.DisplayItem(cStorage);
+
+  Temperature cTemperature(28);
+  Humidity cHumidity(80);
+
+  PrintWeather(cTemperature, cHumidity);
 
   return 0;
 }
